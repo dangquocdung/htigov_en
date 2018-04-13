47566b43596f1f418c05d91ad0fb52914a345f84
Quoc Dung Dang <dungthinhvn@gmail.com>
2018-04-13 15:53:51 +0700

    ss

--- resources/views/frontEnd/includes/menu-side.blade.php
+++ resources/views/frontEnd/includes/menu-side.blade.php
@@ -187,44 +187,50 @@
     </div>
 </div>
 
-<div class="card">
+<div class="block4">
+
+        <div class="card">
       
-        <ul class="nav nav-tabs" role="tablist" style="border-bottom: none">
-
-            <li class="active">
-                
-                <a href="#ty-gia" data-toggle="tab">
-                    
-                    <span class="hidden-xs">
-                        Tỉ giá
-                    </span>
-                    
-                </a>
-            </li>
-            <li>
-                
-                <a href="#gia-vang" data-toggle="tab">
-                    
-                    <span class="hidden-xs">
-                        Giá vàng
-                    </span>
-                    
-                </a>
-            </li>
-                
-        </ul>
-
-        <div class="tab-content">
+                <ul class="nav nav-tabs" role="tablist" style="border-bottom: none">
             
-            <div class="to-chuc tab-pane active" id="ty-gia">
-                ty-gia
+                    <li class="active">
+                        
+                        <a href="#ty-gia" data-toggle="tab">
+                            
+                            <span class="hidden-xs">
+                                Tỉ giá
+                            </span>
+                            
+                        </a>
+                    </li>
+                    <li>
+                        
+                        <a href="#gia-vang" data-toggle="tab">
+                            
+                            <span class="hidden-xs">
+                                Giá vàng
+                            </span>
+                            
+                        </a>
+                    </li>
+                        
+                </ul>
+            
+                <div class="tab-content">
+                    
+                    <div class="to-chuc tab-pane active" id="ty-gia">
+                        ty-gia
+                    </div>
+            
+                    <div class="to-chuc tab-pane" id="gia-vang">
+                        gia-vang
+                    </div>
+            
+                </div>
+            
             </div>
 
-            <div class="to-chuc tab-pane" id="gia-vang">
-                gia-vang
-            </div>
+</div>
 
-        </div>
 
-    </div>
 
