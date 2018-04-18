1fba11a4ae1faf50069a8d876a13fe3c94d79190
Quoc Dung Dang <dungthinhvn@gmail.com>
2018-04-18 08:06:27 +0700

    ss

--- resources/views/frontEnd/includes/head.blade.php
+++ resources/views/frontEnd/includes/head.blade.php
@@ -12,44 +12,29 @@
 <link href="{{ URL::asset('frontEnd/css/jquery.fancybox.css') }}" rel="stylesheet">
 <link href="{{ URL::asset('frontEnd/css/jcarousel.css') }}" rel="stylesheet"/>
 <link href="{{ URL::asset('frontEnd/css/flexslider.css') }}" rel="stylesheet"/>
-
-{{--<link href="{{ URL::asset('frontEnd/css/style.css') }}" rel="stylesheet"/>--}}
-{{--<link href="{{ URL::asset('frontEnd/css/color.css') }}" rel="stylesheet"/>--}}
-
 <link href="{{ URL::asset('frontEnd/css/adminlte.min.css') }}" rel="stylesheet"/>
-
-{{--  <link href="{{ URL::asset('frontEnd/css/style.css') }}" rel="stylesheet"/>  --}}
-
-{{--  <link href="{{ URL::asset('frontEnd/css/app.css') }}" rel="stylesheet"/>  --}}
-
 <link rel="stylesheet" href="{{ mix('frontEnd/css/app.css') }}">
-
-{{--@if( trans('backLang.direction')=="rtl")--}}
-{{--<link href="{{ URL::asset('frontEnd/css/rtl.css') }}" rel="stylesheet"/>--}}
-{{--@endif--}}
-
 <!-- Favicon and Touch Icons -->
 @if(Helper::GeneralSiteSettings("style_fav") !="")
-    <link href="{{ URL::asset('uploads/settings/'.Helper::GeneralSiteSettings("style_fav")) }}" rel="shortcut icon"
-          type="image/png">
+<link href="{{ URL::asset('uploads/settings/'.Helper::GeneralSiteSettings("style_fav")) }}" rel="shortcut icon"
+        type="image/png">
 @else
-    <link href="{{ URL::asset('uploads/settings/nofav.png') }}" rel="shortcut icon" type="image/png">
+<link href="{{ URL::asset('uploads/settings/nofav.png') }}" rel="shortcut icon" type="image/png">
 @endif
 @if(Helper::GeneralSiteSettings("style_apple") !="")
-    <link href="{{ URL::asset('uploads/settings/'.Helper::GeneralSiteSettings("style_apple")) }}" rel="apple-touch-icon">
-    <link href="{{ URL::asset('uploads/settings/'.Helper::GeneralSiteSettings("style_apple")) }}" rel="apple-touch-icon"
-          sizes="72x72">
-    <link href="{{ URL::asset('uploads/settings/'.Helper::GeneralSiteSettings("style_apple")) }}" rel="apple-touch-icon"
-          sizes="114x114">
-    <link href="{{ URL::asset('uploads/settings/'.Helper::GeneralSiteSettings("style_apple")) }}" rel="apple-touch-icon"
-          sizes="144x144">
+<link href="{{ URL::asset('uploads/settings/'.Helper::GeneralSiteSettings("style_apple")) }}" rel="apple-touch-icon">
+<link href="{{ URL::asset('uploads/settings/'.Helper::GeneralSiteSettings("style_apple")) }}" rel="apple-touch-icon"
+        sizes="72x72">
+<link href="{{ URL::asset('uploads/settings/'.Helper::GeneralSiteSettings("style_apple")) }}" rel="apple-touch-icon"
+        sizes="114x114">
+<link href="{{ URL::asset('uploads/settings/'.Helper::GeneralSiteSettings("style_apple")) }}" rel="apple-touch-icon"
+        sizes="144x144">
 @else
-    <link href="{{ URL::asset('uploads/settings/nofav.png') }}" rel="apple-touch-icon">
-    <link href="{{ URL::asset('uploads/settings/nofav.png') }}" rel="apple-touch-icon" sizes="72x72">
-    <link href="{{ URL::asset('uploads/settings/nofav.png') }}" rel="apple-touch-icon" sizes="114x114">
-    <link href="{{ URL::asset('uploads/settings/nofav.png') }}" rel="apple-touch-icon" sizes="144x144">
+<link href="{{ URL::asset('uploads/settings/nofav.png') }}" rel="apple-touch-icon">
+<link href="{{ URL::asset('uploads/settings/nofav.png') }}" rel="apple-touch-icon" sizes="72x72">
+<link href="{{ URL::asset('uploads/settings/nofav.png') }}" rel="apple-touch-icon" sizes="114x114">
+<link href="{{ URL::asset('uploads/settings/nofav.png') }}" rel="apple-touch-icon" sizes="144x144">
 @endif
-
 <script src="{{ URL::asset('frontEnd/js/jquery.js') }}"></script>
 <script src="{{ URL::asset('frontEnd/js/bootstrap.min.js') }}"></script>
 <script src="{{ URL::asset('frontEnd/js/tts.js') }}"></script>
