5e21bda051416a809819d3c7471369f6fd9c2b8d
Quoc Dung Dang <dungthinhvn@gmail.com>
2018-05-02 17:48:04 +0700

    ss

--- resources/views/frontEnd/includes/menu-side.blade.php
+++ resources/views/frontEnd/includes/menu-side.blade.php
@@ -77,29 +77,51 @@
 
 <div class="block4" style="border-radius: 5px">
 
-        <div id="block-header-bd" class="block-header" style="margin-bottom: 0" data-toggle="collapse" href="#ban-do-dia-gioi">
+    <div id="block-header-bd" class="block-header" style="margin-bottom: 0" data-toggle="collapse" href="#ban-do-dia-gioi">
 
-            <h4>
-                <img src="/images/background/lotus.ico" alt="" width="26px"> Thời tiết Hà Tĩnh
-            </h4>
-
-        </div>
-
-        <div class="clearfix"></div>
-
-        <div id="ban-do-dia-gioi" class="panel-collapse collapse in">
-            <a href="http://www.nchmf.gov.vn/web/vi-VN/62/20/28/map/Default.aspx" target="_blank">
-                <div style="padding: 5px 15px">
-
-                    <div id="thoi-tiet" class="thoi-tiet"></div>
-                </div>
-
-                
-            </a>
-        </div>
+        <h4>
+            <img src="/images/background/lotus.ico" alt="" width="26px"> Thời tiết Hà Tĩnh
+        </h4>
 
     </div>
 
+    <div class="clearfix"></div>
+
+    <div id="ban-do-dia-gioi" class="panel-collapse collapse in">
+        <a href="http://www.nchmf.gov.vn/web/vi-VN/62/20/28/map/Default.aspx" target="_blank">
+            <div style="padding: 5px 15px">
+
+                <div id="thoi-tiet" class="thoi-tiet"></div>
+            </div>
+
+            
+        </a>
+    </div>
+
+</div>
+
+<div id="fb-root"></div>
+<script>
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.0&appId=1857385837639663&autoLogAppEvents=1';
+        fjs.parentNode.insertBefore(js, fjs);
+        }
+        (document, 'script', 'facebook-jssdk')
+    );
+</script>
+
+<div class="block4">
+
+    <div class="fb-page" data-href="https://www.facebook.com/dulichbienhatinh/" 
+        >
+        <blockquote cite="https://www.facebook.com/dulichbienhatinh/" class="fb-xfbml-parse-ignore">
+            <a href="https://www.facebook.com/dulichbienhatinh/">Du Lịch Biển Hà Tĩnh</a>
+        </blockquote>
+    </div>
+</div>
 
 
     
@@ -214,6 +236,15 @@
         </div>
     </div>
 </div>
+
+
+<div class="clearfix"></div>
+<iframe frameborder="0" src="//www.tygia.com/api.php?column=1&amp;title=0&amp;chart=0&amp;gold=1&amp;rate=0&amp;expand=0&amp;color=B4D0D0&amp;titlecolor=333333&amp;nganhang=VIETCOM&amp;fontsize=80&amp;change=0&amp;css=%23SJC_N_ng{display:%20table-row%20!important;}" style="border: none;" width="100%" height="100%"></iframe>
+
+<div class="clearfix"></div>
+<iframe frameborder="0" src="//www.tygia.com/api.php?column=1&amp;title=0&amp;chart=0&amp;gold=0&amp;rate=1&amp;ngoaite=usd,jpy,chf,eur,gbp,aud&amp;expand=0&amp;color=B4D0D0&amp;titlecolor=333333&amp;bgcolor=ffffff&amp;upcolor=00aa00&amp;downcolor=bb0000&amp;textcolor=333333&amp;nganhang=VIETCOM&amp;fontsize=80&amp;ngay=" style="border: none;" width="100%" height="100%"></iframe>
+        
+   
 
 
 {{--  <div class="card">
