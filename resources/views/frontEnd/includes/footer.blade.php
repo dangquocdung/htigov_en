515e53149ea3c2c678d64a9ec614d36af7ad586a
Quoc Dung Dang <dungthinhvn@gmail.com>
2018-12-17 16:50:37 +0700

    adsad

--- resources/views/frontEnd/includes/footer.blade.php
+++ resources/views/frontEnd/includes/footer.blade.php
@@ -31,6 +31,11 @@
                         </p>
                     </div>
                 </div>
+
+                <div class="zalo-chat-widget" data-oaid="4156375982830930462" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="350" data-height="420"></div>
+
+                <script src="https://sp.zalo.me/plugins/sdk.js"></script>
+
             </div>
         </div>
     </div>
