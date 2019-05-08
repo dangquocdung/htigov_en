3cfec459be662ce5839a93ee851490cf0ce23342
dangquocdung <dungthinhvn@gmail.com>
2019-05-08 07:49:24 +0700

    ádsad

--- vendor/inani/larapoll/src/views/stubs/radio.blade.php
+++ vendor/inani/larapoll/src/views/stubs/radio.blade.php
@@ -1,28 +1,28 @@
 <form method="POST" action="{{ route('poll.vote', $id) }}" >
-        @csrf
-        <div class="">
-            <div class="" style="padding-top: 5px">
-                <h5 class="panel-title" style="line-height: 1.5em">
-                    {{ $question }}
-                </h5>
-            </div>
+    @csrf
+    <div class="">
+        <div class="" style="padding-top: 5px">
+            <h5 class="panel-title" style="line-height: 1.5em">
+                {{ $question }}
+            </h5>
         </div>
-        <div class="">
-            <ul class="list-group" style="padding: 0 10px">
-                @foreach($options as $id => $name)
-                    <li class="" style="padding: 0 15px">
-                        <div class="radio" style="text-align:left; padding: 5px">
-                            <label>
-                                <input value="{{ $id }}" type="radio" name="options">
-                                {{ $name }}
-                            </label>
-                        </div>
-                    </li>
-                @endforeach
-            </ul>
-        </div>
-        <div class="" style="margin-bottom: 20px">
-            <input type="submit" class="btn btn-primary btn-sm" value="Vote" />
-        </div>
-    </form>
+    </div>
+    <div class="">
+        <ul class="list-group" style="padding: 0 10px">
+            @foreach($options as $id => $name)
+                <li class="" style="padding: 0 15px">
+                    <div class="radio" style="text-align:left; padding: 5px">
+                        <label>
+                            <input value="{{ $id }}" type="radio" name="options">
+                            {{ $name }}
+                        </label>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+    <div class="" style="margin-bottom: 20px">
+        <input type="submit" class="btn btn-primary btn-sm" value="Bình chọn" />
+    </div>
+</form>
     
