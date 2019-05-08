950865baa806579d3ea30e5b3bd15fd41de4b843
dangquocdung <dungthinhvn@gmail.com>
2019-05-08 08:03:23 +0700

    asdas

--- vendor/inani/larapoll/src/views/stubs/results.blade.php
+++ vendor/inani/larapoll/src/views/stubs/results.blade.php
@@ -21,7 +21,8 @@
 
 @foreach($options as $option)
     <div class='result-option-id'>
-        <strong>{{ $option->name }}</strong><span class='pull-right'>{{ $option->percent }}%</span>
+        <span style="text-align:left">{{ $option->name }}</span>
+        <span class='pull-right'>{{ $option->percent }}%</span>
         <div class='progress'>
             <div class='progress-bar progress-bar-striped active' role='progressbar' aria-valuenow='{{ $option->percent }}' aria-valuemin='0' aria-valuemax='100' style='width: {{ $option->percent }}%'>
                 <span class='sr-only'>{{ $option->percent }}% Complete</span>
