9b04e5cc83b25c0416798dce6a1e346965e0b662
dangquocdung <dungthinhvn@gmail.com>
2019-05-12 18:30:40 +0700

    asdsa

--- config/larapoll_config.php
+++ config/larapoll_config.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'admin_auth' => env('LARAPOLL_ADMIN_AUTH_MIDDLEWARE', 'managerlevel'),
-    'admin_guard' => env('LARAPOLL_ADMIN_AUTH_GUARD', 'auth'),
+    'admin_guard' => env('LARAPOLL_ADMIN_AUTH_GUARD', 'web'),
     'pagination' => env('LARAPOLL_PAGINATION', 15),
     'prefix' => env('LARAPOLL_PREFIX', 'tham-do-y-kien'),
     'results' => '',
