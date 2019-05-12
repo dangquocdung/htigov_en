33c2df149c830a793ee7613265e61117a3a83e1f
dangquocdung <dungthinhvn@gmail.com>
2019-05-12 16:41:45 +0700

    sadsa

--- resources/views/frontEnd/includes/menu-side.blade.php
+++ resources/views/frontEnd/includes/menu-side.blade.php
@@ -160,7 +160,7 @@
             @if (Auth::guest())
 
             <div class="clearfix"></div>
-            <em>Bạn chưa <a href="{{ url('login') }}" style="color:red; text-decoration:">đăng nhập</a></em>
+            <em>Bạn chưa <a href="{{ url('login') }}" style="color:red;">đăng nhập</a></em>
 
             @endif
 
