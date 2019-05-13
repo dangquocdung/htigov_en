3a19ee7279b6fc1b5cccefc9ae193fab9a28f0e3
dangquocdung <dungthinhvn@gmail.com>
2019-05-13 15:14:26 +0700

    asdsa

--- resources/views/frontEnd/includes/toolbox.blade.php
+++ resources/views/frontEnd/includes/toolbox.blade.php
@@ -68,21 +68,23 @@
             <a href="{{ URL::to("admin") }}" title="{{trans('frontLang.dashboard')}}">
 
                 
-                @if (Auth::check())
+                {{-- @if (Auth::check())
 
                     <span class="avatar w-32">
-                        {{-- @if(Auth::user()->photo !="")
+                        @if(Auth::user()->photo !="")
                             <img src="{{ URL::to('uploads/users/'.Auth::user()->photo) }}" alt="{{ Auth::user()->name }}"
                                 title="{{ Auth::user()->name }}">
-                        @else --}}
+                        @else
                             <img src="{{ URL::to('backEnd/assets/images/profile.jpg') }}" alt="{{ Auth::user()->name }}"
                                 title="{{ Auth::user()->name }}">
-                        {{-- @endif --}}
+                        @endif
                     </span>
 
                 @else
                     <span><i class="fa fa-user-circle-o"></i></span>
-                @endif
+                @endif --}}
+
+                <span><i class="fa fa-user-circle-o"></i></span>
             </a>
         </li>
         
