b2f2ab2908dd5aed63560259ab27ef5ccb66d441
dangquocdung <dungthinhvn@gmail.com>
2019-05-17 18:11:30 +0700

    asdsa

--- packages/inani/larapoll/src/Http/routes.php
+++ packages/inani/larapoll/src/Http/routes.php
@@ -9,7 +9,12 @@ Route::group(['namespace' => 'Inani\Larapoll\Http\Controllers', 'prefix' => $pre
     $guard = config('larapoll_config.admin_guard');
 
     Route::middleware(["$middleware:$guard"])->group(function () {
-        Route::get('/admin', 'PollManagerController@index')->name('poll.home');
+        // Route::get('/admin', 'PollManagerController@index')->name('poll.home');
+        Route::get('/admin', function(){
+            echo $middleware.'-'.$guard;
+        });
+
+
         Route::get('/admin/polls', ['uses' => 'PollManagerController@index', 'as' => 'poll.index']);
         Route::get('/admin/polls/create', ['uses' => 'PollManagerController@create', 'as' => 'poll.create']);
         Route::get('/admin/polls/{poll}', ['uses' => 'PollManagerController@edit', 'as' => 'poll.edit']);
