d1fc1bf3de1d9c1bfc1c9d6b491a74997bb9097b
dangquocdung <dungthinhvn@gmail.com>
2019-05-17 17:33:17 +0700

    sadasd

--- resources/views/frontEnd/includes/menu-side.blade.php
+++ resources/views/frontEnd/includes/menu-side.blade.php
@@ -153,7 +153,7 @@
 
     <div class="box-banner box-tham-do" style="text-align:center; padding: 5px">
 
-        {{-- {{ PollWriter::draw(2) }} --}}
+        {{ PollWriter::draw(2) }}
 
     </div>
 
