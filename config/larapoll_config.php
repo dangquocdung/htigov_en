612fff54a4de68f362b3b0576040015fb7a05868
dangquocdung <dungthinhvn@gmail.com>
2019-05-17 08:12:22 +0700

    áds

--- config/larapoll_config.php
+++ config/larapoll_config.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'admin_auth' => env('LARAPOLL_ADMIN_AUTH_MIDDLEWARE', 'auth'),
+    'admin_auth' => env('LARAPOLL_ADMIN_AUTH_MIDDLEWARE', 'managerlevel'),
     'admin_guard' => env('LARAPOLL_ADMIN_AUTH_GUARD', 'web'),
     'pagination' => env('LARAPOLL_PAGINATION', 15),
     'prefix' => env('LARAPOLL_PREFIX', 'admin_polls'),
